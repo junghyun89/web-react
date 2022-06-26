ee0f4556c0d4dcd5adb8a4ff153c2c80337b03bf
jung89 <104253822+junghyun89@users.noreply.github.com>
2022-06-26 14:44:14 +0900

    learning

---
+++ 3.useEffect/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  useEffect(() => {
+    console.log('I run only once.');
+  }, []);
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 5) {
+      console.log('I run when keyword changes.');
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log('I run when counter changes.');
+  }, [counter]);
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+};
+
+export default App;
